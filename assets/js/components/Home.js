9d73a02e6243b55f89a786b1b2b850ba196a66d5
Theofilos Karachristos <tkarachristos@gmail.com>
2019-10-06 09:00:48 +0300

    Added a react-bootstrap form with an email control

--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+import Form from 'react-bootstrap/Form';
+
 export default class Home extends Component {
     render() {
         return (
-            <h3>Yo yo Theofilos - welcome to Encore!</h3>
+            <div>
+                <h3>Yo yo Theofilos - welcome to Encore!</h3>
+
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+                </Form>
+            </div>
         )
     };
 }
