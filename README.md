286bf8d35750b2d03fb8dddc71a31acca72c5d9b
Theofilos Karachristos <tkarachristos@gmail.com>
2019-10-07 06:24:44 +0300

    Adding state and form controls to Home component

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ The results are sent to the provided email.
 The application uses the Symfony framework.
 
 Symfony goodies used: Routing annotations
+
+Prerequisites:
+- PHP
+- Composer
+- NPM
+
+Running the application:
+1) Run "composer install"
+2) Run "npm install"
+3) Run "npm run build" and press "Ctrl+c" after completion (give "y" for terminating the batch job)
+4) Run "php -S 127.0.0.1:8000 -t public"
+5) Navigate to http://localhost:8000/
--- assets/js/components/Home.js
+++ assets/js/components/Home.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
-
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import DatePicker from 'react-datepicker';
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            companySymbol: "",
+            companySymbolValid: false,
+            startDate: new Date(),
+            startDateValid: true,
+            endDate: new Date(),
+            endDateValid: true,
+            email: "",
+            emailValid: false
+        };
+
+        this.handleChangeStartDate = this.handleChangeStartDate.bind(this);
+        this.handleChangeEndDate = this.handleChangeEndDate.bind(this);
+    }
+
+    handleChangeStartDate(date) {
+        this.setState({
+            startDate: date // ISO String, ex: "2016-11-19T12:00:00.000Z"
+        });
+    }
+
+    handleChangeEndDate(date) {
+        this.setState({
+            endDate: date // ISO String, ex: "2016-11-19T12:00:00.000Z"
+        });
+    }
+
     render() {
         return (
             <div>
-                <h3>Yo yo Theofilos - welcome to Encore!</h3>
+                <h2>Historical Quotes Finder</h2>
 
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
+                <Form noValidate >
+                    <Form.Group controlId="formCompanySymbol">
+                        <Form.Label>Company Symbol</Form.Label>
+                        <Form.Control placeholder="Enter company symbol" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formStartDate">
+                        <Form.Label>Start Date</Form.Label>
+                        <DatePicker id="startDate" selected={this.state.startDate} onChange={this.handleChangeStartDate} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formEndDate">
+                        <Form.Label>End Date</Form.Label>
+                        <DatePicker id="endDate" selected={this.state.endDate} onChange={this.handleChangeEndDate} />
+                    </Form.Group>
+
+                    <Form.Group controlId="formEmail">
                         <Form.Label>Email address</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
                     </Form.Group>
-                </Form>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>           
             </div>
         )
     };
